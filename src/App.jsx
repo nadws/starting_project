5aef95928c0da4f42fb0e44e3040df068c099a54
nadws <nandw567@gmai.com>
2025-07-10 22:16:06 +0800

    tab content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [selectedToppic , setSelectedToppic]=useState();
+  function handleSelect(selectedButton){
+    setSelectedToppic(selectedButton);
+  }
+  let tabContent = <p>Plese selected toppic</p>;
+  if (selectedToppic) {
+    tabContent = <div id="tab-content">
+            <h3>{EXAMPLES[selectedToppic].title}</h3>
+            <p>{EXAMPLES[selectedToppic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedToppic].code}</code>
+            </pre>
+          </div>;
+  }
   return (
     <div>
       <Header />
@@ -20,11 +36,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
